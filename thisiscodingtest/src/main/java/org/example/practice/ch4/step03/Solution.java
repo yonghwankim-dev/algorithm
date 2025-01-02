2a25d81bb47ce5e4f46bb2aeb06dff5cba9a6e88
yonghwankim-dev <dragonbead95@naver.com>
2025-01-02 10:58:31 +0900

    feat: 왕실의 나이트

--- thisiscodingtest/src/main/java/org/example/practice/ch4/step03/Solution.java
+++ thisiscodingtest/src/main/java/org/example/practice/ch4/step03/Solution.java
@@ -1,36 +1,35 @@
 package org.example.practice.ch4.step03;
 
 public class Solution {
-	public static int solution(String position){
-		String[] split = position.split("");
-		String cols = "abcdefgh";
+	public static void main(String[] args) {
+		assert solution("a1") == 2;
+		assert solution("c3") == 8;
+	}
+
+	private static int solution(String position){
+		int result = 0;
 		int n = 8;
-		int x = cols.indexOf(split[0]) + 1;
-		int y = Integer.parseInt(split[1]);
 
 		// 상좌, 상우, 하좌, 하우, 좌상, 좌하, 우상, 우하
-		int[] dy = {-2, -2, 2, 2, -1, 1, -1, 1};
-		int[] dx = {-1, 1, -1, 1, -2, -2, 2, 2};
-		int answer = 0;
+		int[][] dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {1, -2}, {-1, 2}, {1, 2}};
 
-		for (int i = 0; i < n; i++){
-			int ny = dy[i] + y;
-			int nx = dx[i] + x;
+		String[] params = position.split("");
+		int y = Integer.parseInt(params[1]);
+		int x = parseCol(params[0]);
 
+		for (int[] dir : dirs){
+			int ny = dir[0] + y;
+			int nx = dir[1] + x;
 			if (ny <= 0 || ny > n || nx <= 0 || nx > n){
 				continue;
 			}
-			answer++;
+			result++;
 		}
-
-		return answer;
+		System.out.println(position + ": " + result);
+		return result;
 	}
-	public static void main(String[] args) {
-		int answer = solution("a1");
-		assert answer == 2;
 
-		answer = solution("c2");
-		System.out.println(answer);
-		assert answer == 6;
+	private static int parseCol(String text){
+		return text.toLowerCase().charAt(0) - 'a' + 1;
 	}
 }
